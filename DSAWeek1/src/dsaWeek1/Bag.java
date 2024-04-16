5c66697c2a270edcc07674b60807992d9a21eab5
paramoody <147210380+paramoody@users.noreply.github.com>
2024-04-16 09:38:48 -0600

    dsa week 1

--- DSAWeek1/src/dsaWeek1/Bag.java
+++ DSAWeek1/src/dsaWeek1/Bag.java
@@ -2,12 +2,11 @@ package dsaWeek1;
 
 public class Bag<T> implements BagInterface<T>{
 
+	//Internal node class creates the structure for the linked list used by the bag
 	private class Node<E>{
 		public E item;
 		public Node<E> next;
-		//public Node(E item) {
-		//	this(item, null);
-		//}
+		
 		public Node(E item, Node<E> next) {
 			this.item = item;
 			this.next = next;
@@ -18,53 +17,52 @@ public class Bag<T> implements BagInterface<T>{
 	private int numberOfItems;
 	private Node<T> first;
 	
+	//Constructor creates a bag object 
 	public Bag() {
 		first = null;
 		numberOfItems = 0;
 	}
 
-	
+	//Add method adds an item to the bag, and sets that item to first in the linked list
 	@Override
-	public boolean add(T item) {
+	public void add(T item) {
 		Node<T> newNode = new Node<>(item, first);
 		first = newNode;
 		numberOfItems++;
-		return true;
 	}
-
+	//Unparameterized remove method removes the first item in the list
 	@Override
-	public T remove() {
+	public void remove() {
 		if (numberOfItems == 0) {
 			throw new IllegalArgumentException("Bag is Empty");
 		}
-		Node<T> toRemove = first;
 		first = first.next;
-		return toRemove.item;
+		
 	}
-
+	//parameterized remove method searches through list until it finds the item, then removes it.
 	@Override
-	public boolean remove(T item) {
+	public void remove(T item) {
 		if (numberOfItems == 0) {
 			throw new IllegalArgumentException("Bag is Empty");
 		}
 		if(item.equals(first.item)) {
 			remove();
-			return true;
+			
 			
 		}
 		Node<T> current = first;
 		while(current.next != null) {
 			if(current.next.item.equals(item)) {
 				current.next = current.next.next;
-				return true;
+				
 			}
 			current = current.next;
 		}
-		return false;
+		
 	}
 
 	
-
+	//Count method searches through the entire list, and counts the occurances of an item
 	@Override
 	public int count(T item) {
 		int frequency = 0;
@@ -77,7 +75,7 @@ public class Bag<T> implements BagInterface<T>{
 		}
 		return frequency;
 	}
-
+	//contains method searches until it finds the item it is looking for, and returns a boolean value
 	@Override
 	public boolean contains(T item) {
 		Node<T> current = first;
@@ -90,7 +88,7 @@ public class Bag<T> implements BagInterface<T>{
 		}
 		return false;
 	}
-
+	//toArray creates an array of type T holding all items in the bag
 	@Override
 	public T[] toArray() {
 		Node<T> current = first;
--- DSAWeek1/src/dsaWeek1/BagInterface.java
+++ DSAWeek1/src/dsaWeek1/BagInterface.java
@@ -1,9 +1,9 @@
 package dsaWeek1;
 
 public interface BagInterface<T> {
-	public boolean add(T item);
-	public T remove();
-	public boolean remove(T item);
+	public void add(T item);
+	public void remove();
+	public void remove(T item);
 	public int count(T item);
 	public boolean contains(T item);
 	public T[] toArray();
